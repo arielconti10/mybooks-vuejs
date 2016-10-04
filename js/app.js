848363f4fd0205816506f9dcf5d4d794e6e652e5
Soul Digital <ariel@souldigital.com.br>
2016-10-04 19:02:43 -0300

    Iniciando paginacao

--- js/app.js
+++ js/app.js
@@ -4,12 +4,39 @@
         books:[],
         mySearch: '',
         orderCol: 'id',
-        orderInverse: 1
+        orderInverse: 1,
+        pagination: {
+            maxPage:4,
+            current:1,
+            totalItens:0,
+            totalPages:5,
+            listNumbers:[],
+            listPagination:[]
+        }
      },
      methods:{
          filterOrderBy:function(e, col){
              this.orderCol = col;
              this.orderInverse = this.orderInverse * -1;
+         },
+         previous: function(e){
+            e.preventDefault();
+
+             if(this.pagination.current === 1){
+                 return false;
+             }
+             this.pagination.current = this.pagination.current - 1;
+         },
+         pagePagination:function(e, id){
+             e.preventDefault();
+
+         },
+         next: function (){
+             e.preventDefault();
+             if(this.pagination.current === this.pagination.totalPages){
+                 return false;
+             }
+             this.pagination.current = this.pagination.current +1 ;
          }
      },
      ready:function(){
