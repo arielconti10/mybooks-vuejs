7d92394c62329a19d01ddc23771ece54b1f59082
Ariel conti <ariel@souldigital.com.br>
2016-10-06 00:31:03 -0300

    Finalizando paginação com Vue JS

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
             maxPage:4,
             current:1,
             totalItens:0,
-            totalPages:5,
+            totalPages:0,
             listNumbers:[],
             listPagination:[]
         }
@@ -20,29 +20,72 @@
              this.orderInverse = this.orderInverse * -1;
          },
          previous: function(e){
-            e.preventDefault();
+             e.preventDefault();
+
+
+             console.log('maxPage: ' + this.pagination.maxPage);
+             console.log('current: ' + this.pagination.current);
+             console.log('totalItems: ' + this.pagination.totalItems);
+             console.log('totalPages: ' + this.pagination.totalPages);
+             console.log('listPagination: ' + this.pagination.listPagination);
 
              if(this.pagination.current === 1){
                  return false;
              }
              this.pagination.current = this.pagination.current - 1;
-         },
-         pagePagination:function(e, id){
-             e.preventDefault();
+
+             this.books = this.pagination.listPagination[this.pagination.current - 1];
 
          },
-         next: function (){
+         pagePagination:function(e, current){
              e.preventDefault();
+             console.log('maxPage: ' + this.pagination.maxPage);
+             console.log('current: ' + this.pagination.current);
+             console.log('totalItems: ' + this.pagination.totalItems);
+             console.log('totalPages: ' + this.pagination.totalPages);
+             console.log('listPagination: ' + this.pagination.listPagination);
+             this.pagination.current = current + 1;
+
+             this.books = this.pagination.listPagination[current];
+
+
+         },
+         next:function(e){
+             e.preventDefault();
+             console.log('maxPage: ' + this.pagination.maxPage);
+             console.log('current: ' + this.pagination.current);
+             console.log('totalItems: ' + this.pagination.totalItems);
+             console.log('totalPages: ' + this.pagination.totalPages);
+             console.log('listPagination: ' + this.pagination.listPagination);
+
              if(this.pagination.current === this.pagination.totalPages){
                  return false;
              }
-             this.pagination.current = this.pagination.current +1 ;
+             this.pagination.current = this.pagination.current + 1;
+
+             this.books = this.pagination.listPagination[this.pagination.current - 1];
+
          }
      },
      ready:function(){
          var self = this;
          self.$http.get('dataServer.json').then(function(response){
-             self.books = response.data;
+             self.pagination.totalItens = response.data.length;
+             self.pagination.totalPages = Math.ceil(self.pagination.totalItens / self.pagination.maxPage);
+             var aux = [];
+             for(var k in response.data){
+                 aux.push(response.data[k]);
+                 if(aux.length === self.pagination.maxPage){
+                     self.pagination.listPagination.push(aux);
+                     aux = [];
+                 }
+             }
+             if(aux.length > 0){
+                 self.pagination.listPagination.push(aux);
+             }
+             console.log(self.pagination.listPagination);
+
+             self.books = self.pagination.listPagination[0];
          });
      }
  });
